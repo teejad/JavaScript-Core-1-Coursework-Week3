114c98558e37330aa43dbf5ae4106a5e4cedf41c
Teejad <65988841+teejad@users.noreply.github.com>
2021-09-19 11:22:05 +0100

    WM3-HIBAH
    H G I J = fixed

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -10,7 +10,17 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 /* EXPECTED OUTPUT */
-
+let fizzBuzz = arr.forEach((number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else if (number % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(number);
+  }
+});
 /*
 1
 2
